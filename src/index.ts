9b46c1cb9f123c44d30b89e3bbff2a356a0d5489
Mike Wu <mike@bitguild.com>
2020-10-13 00:05:02 +0800

    feat: pipeline

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 type Func = () => any;
+type AnyFunc = (...args: any[]) => any;
 
 interface Options {
   onErrorOccur?: 'skip' | 'break';
@@ -9,6 +10,14 @@ interface Options {
 type OnOneDone = (r: any, success: number, fail: number, total: number) => void;
 type OnError = (err: Error, success: number, fail: number, total: number) => void;
 type OnAllDone = (success: number, fail: number, total: number) => void;
+
+interface Monitor {
+  onProgress: OnOneDone;
+  onError: OnError;
+  onDone: OnAllDone;
+}
+
+export type Job = Partial<Monitor> & AnyFunc;
 
 export class Concurrent {
 
@@ -147,4 +156,96 @@ export class Concurrent {
 
 export default function concurrent(limit: number, options?: Options) {
   return new Concurrent(limit, options);
+}
+
+class PinelineRunner {
+
+  job: Job;
+  concurr: number;
+  flatten: boolean;
+  all: any[];
+  q: Concurrent;
+
+  constructor(
+    {job, concurr, flatten, all}: {job: Job, concurr: number, flatten: boolean, all: any[]}
+  ) {
+    this.job = job;
+    this.concurr = concurr;
+    this.flatten = flatten;
+    this.all = all;
+    this.q = concurrent(concurr);
+    if (job.onProgress) {
+      this.q.one(job.onProgress);
+    }
+    if (job.onError) {
+      this.q.error(job.onError);
+    }
+    if (job.onDone) {
+      this.q.done(job.onDone);
+    }
+  }
+
+  async start(...args: any[]) {
+    if (this.flatten) {
+      for (const arg of args[0]) {
+        this.q.go(async () => {
+          await this.job(arg, (...nextArgs: any[]) => {
+            const next = this.all[this.all.indexOf(this) + 1];
+            next.start(...nextArgs);
+          });
+        });
+      }
+    } else {
+      this.q.go(async () => {
+        await this.job(...args, (...nextArgs: any[]) => {
+          const next = this.all[this.all.indexOf(this) + 1];
+          next.start(...nextArgs);
+        });
+      });
+    }
+  }
+
+}
+
+export class Pipeline {
+
+  runners: PinelineRunner[];
+
+  constructor() {
+    this.runners = [];
+  }
+
+  pipe(job: Job, concurr= 1) {
+    this.runners.push(new PinelineRunner({
+      job,
+      concurr,
+      all: this.runners,
+      flatten: false
+    }));
+    return this;
+  }
+
+  pipeAll(job: Job, concurr= 1) {
+    this.runners.push(new PinelineRunner({
+      job,
+      concurr,
+      flatten: true,
+      all: this.runners
+    }));
+    return this;
+  }
+
+  start(job: Job, concurr= 1) {
+    this.runners.unshift(new PinelineRunner({
+      job,
+      concurr,
+      flatten: false,
+      all: this.runners
+    }));
+    setTimeout(() => {
+      this.runners[0].start();
+    }, 0);
+    return this;
+  }
+
 }
